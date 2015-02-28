a614a6fc140cb72d0f517f51337075c9ed4fceac
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-02-28 19:29:41 +0100

    Should configure the client with url.

--- lib/sagrone/client.rb
+++ lib/sagrone/client.rb
@@ -1,4 +1,13 @@
+require 'singleton'
+
 module Sagrone
   class Client
+    include Singleton
+
+    attr_accessor :url
+
+    def configure
+      yield self
+    end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe Sagrone::Client do
+  let(:client) { Sagrone::Client.instance }
+
+  it 'should configure the client' do
+    client.configure do |c|
+      c.url = 'http://api.sagrone/v1'
+    end
+
+    expect(client.url).to eq 'http://api.sagrone/v1'
+  end
 end
