22d34475af3114013ca01da074dce58c4c41b942
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-02 23:17:14 +0200

    Add describe block for configure method.

--- spec/sagrone/client_spec.rb
+++ spec/sagrone/client_spec.rb
@@ -3,21 +3,23 @@ require 'spec_helper'
 RSpec.describe Sagrone::Client do
   let(:client) { Sagrone::Client.instance }
 
-  it 'should configure the client' do
-    client.configure do |c|
-      c.url = 'http://api.sagrone/v1'
+  describe 'self.configure' do
+    it 'should configure the client' do
+      client.configure do |c|
+        c.url = 'http://api.sagrone/v1'
+      end
+
+      expect(client.api).to be_a(Her::API)
+      expect(client.url).to eq 'http://api.sagrone/v1'
+      expect(client.api.base_uri).to eq 'http://api.sagrone/v1'
     end
 
-    expect(client.api).to be_a(Her::API)
-    expect(client.url).to eq 'http://api.sagrone/v1'
-    expect(client.api.base_uri).to eq 'http://api.sagrone/v1'
-  end
+    it 'should have a configurable url' do
+      client.configure do |c|
+        c.url = 'http://different-endpoint'
+      end
 
-  it 'should have a configurable url' do
-    client.configure do |c|
-      c.url = 'http://different-endpoint'
+      expect(client.api.base_uri).to eq 'http://different-endpoint'
     end
-
-    expect(client.api.base_uri).to eq 'http://different-endpoint'
   end
 end
