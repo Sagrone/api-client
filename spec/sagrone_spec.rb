a0a7cd743e1e8d4b4ba3b3a93e89e4df86e96f75
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-05-02 19:41:46 +0200

    Describe Sagrone.client block.

--- spec/sagrone_spec.rb
+++ spec/sagrone_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 RSpec.describe Sagrone do
-  it 'should fetch a client' do
-    expect(Sagrone.client).to be_a(Sagrone::Client)
+  describe '.client' do
+    it 'should fetch a client' do
+      expect(Sagrone.client).to be_a(Sagrone::Client)
+    end
   end
 end
